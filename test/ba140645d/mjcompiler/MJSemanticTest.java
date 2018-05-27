4abf03e467394f4c60984d6b3bc9553f794aca1f
AndrijaLFC <andrija.boricic@hotmail.com>
2018-05-27 20:49:42 +0200

    Added word doc and tests

--- test/ba140645d/mjcompiler/MJSemanticTest.java
+++ test/ba140645d/mjcompiler/MJSemanticTest.java
@@ -1,6 +1,41 @@
 package  ba140645d.mjcompiler;
 
 
-class MJSemanticTest {
+import ba140645d.log4j.Log4JUtil;
+import java_cup.runtime.Symbol;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
+import java.io.*;
+
+class MJSemanticTest {
+    static {
+        DOMConfigurator.configure(Log4JUtil.instance().findLoggerConfigFile());
+        Log4JUtil.instance().prepareLogFile(Logger.getRootLogger());
+    }
+
+    public static void main(String[] args) throws Exception {
+        Logger log = Logger.getLogger(MJLexerTest.class);
+        Reader bufferReader = null;
+
+        try{
+            File source = new File("test/testProgram.mj");
+            log.info("Compiling source file :" + source.getAbsolutePath());
+
+            bufferReader = new BufferedReader(new FileReader(source));
+
+            Yylex lexer = new Yylex(bufferReader);
+
+            Parser parser = new Parser(lexer);
+
+            Symbol symbol = parser.parse();
+
+
+
+        } catch (FileNotFoundException e) {
+
+        } finally{
+            if (bufferReader != null) try { bufferReader.close(); } catch (IOException exc) { exc.printStackTrace(); }
+        }
+    }
 }
